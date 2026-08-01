ddd5b9c6cb960dbc03b5778a5fb4a4b96b89f776
Rida1019-taki <takirida72@gmail.com>
2026-08-01 21:38:16 +0100

    security

--- src/main/java/org/elogitrack/logitrack/Service/CustomUserDetailsService.java
+++ src/main/java/org/elogitrack/logitrack/Service/CustomUserDetailsService.java
@@ -1,6 +1,10 @@
 package org.elogitrack.logitrack.service;
 
+import org.elogitrack.logitrack.model.User;
+import org.elogitrack.logitrack.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +18,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email);
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));
         if (user == null){
             throw new UsernameNotFoundException("User not found with email: " + email);
         }
--- src/main/java/org/elogitrack/logitrack/security/AuthEntryPointJwt.java
+++ src/main/java/org/elogitrack/logitrack/security/AuthEntryPointJwt.java
@@ -3,6 +3,7 @@ package org.elogitrack.logitrack.security;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import javax.naming.AuthenticationException;
@@ -12,10 +13,7 @@ import java.io.IOException;
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest request,
-                         HttpServletResponse response,
-                         AuthenticationException authException)
-            throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.AuthenticationException authException) throws IOException, ServletException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED , "401 Unauthorized");
     }
 }
